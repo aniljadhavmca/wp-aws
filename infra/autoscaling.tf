fe42e653bd04e443d7df00d21f32810f64caad02
Anil Jadhav <aniljadhav@gmail.com>
2026-06-29 16:10:14 +0530

    fix: move EC2 to public subnet (sandbox SCP restriction)

--- infra/autoscaling.tf
+++ infra/autoscaling.tf
@@ -4,10 +4,10 @@
 resource "aws_instance" "wordpress" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.ec2_instance_type
-  subnet_id                   = aws_subnet.private_a.id
+  subnet_id                   = aws_subnet.public_a.id
   vpc_security_group_ids      = [aws_security_group.ec2.id]
   iam_instance_profile        = aws_iam_instance_profile.ec2.name
-  associate_public_ip_address = false
+  associate_public_ip_address = true
 
   root_block_device {
     volume_size           = var.ec2_volume_size
